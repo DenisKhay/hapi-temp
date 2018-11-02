cba70b92c9996d62120f79daece15c82284f8485
denis.khaydarshin <dragolietel@gmail.com>
2018-11-03 01:29:51 +0300

    added gql

---
+++ graphql/PaintingType.js
@@ -0,0 +1,16 @@
+const grapql = require('graphql');
+const { GraphQLObjectType, GraphQLString } = grapql;
+
+const PaintingType = new GraphQLObjectType({
+    name: 'Painting',
+    fields: ()=>{
+        return {
+            id: { type: GraphQLString },
+            name: { type: GraphQLString },
+            url: { type: GraphQLString },
+            techniques: { type: GraphQLString }
+        }
+    }
+});
+
+module.exports = PaintingType;
